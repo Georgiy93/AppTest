17845d662860f515529b4200852bd97b9db45b1c
Georgiy Sagitov <georgmc@yandex.ru>
2023-11-02 23:04:21 +0300

    Fix code

--- app/src/main/java/ru/trueconf/apptest/MainActivity.kt
+++ app/src/main/java/ru/trueconf/apptest/MainActivity.kt
@@ -11,7 +11,7 @@ import android.graphics.Color
 import android.os.Build
 
 
-import android.util.Log
+
 import android.view.MotionEvent
 
 import androidx.constraintlayout.widget.ConstraintLayout
@@ -23,6 +23,7 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.suspendCancellableCoroutine
 import ru.trueconf.apptest.databinding.ActivityMainBinding
+import kotlin.coroutines.resume
 
 class MainActivity : AppCompatActivity() {
 
@@ -36,109 +37,93 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val layoutParams = binding.hello.layoutParams as ConstraintLayout.LayoutParams
-        layoutParams.startToStart = ConstraintLayout.LayoutParams.UNSET
-        layoutParams.endToEnd = ConstraintLayout.LayoutParams.UNSET
-        layoutParams.topToTop = ConstraintLayout.LayoutParams.UNSET
-        layoutParams.bottomToBottom = ConstraintLayout.LayoutParams.UNSET
-        binding.hello.layoutParams = layoutParams
-        binding.hello.setOnTouchListener { _, event ->
-            if (event.action == MotionEvent.ACTION_DOWN) {
-
-                animator?.cancel()
-                lifeCycleCoroutine?.cancel()
-                true
-            } else {
-                false
+        setupHelloTextView()
+        setupTouchListeners()
+    }
+        private fun setupHelloTextView() {
+            val layoutParams = binding.hello.layoutParams as ConstraintLayout.LayoutParams
+            layoutParams.apply {
+                startToStart = ConstraintLayout.LayoutParams.UNSET
+                endToEnd = ConstraintLayout.LayoutParams.UNSET
+                topToTop = ConstraintLayout.LayoutParams.UNSET
+                bottomToBottom = ConstraintLayout.LayoutParams.UNSET
+            }
+            binding.hello.layoutParams = layoutParams
+        }
+    private fun setupTouchListeners() {
+        binding.hello.setOnTouchListener { _, event -> handleHelloTouch(event) }
+        binding.parentLayout.setOnTouchListener { _, event -> handleParentLayoutTouch(event) }
+    }
+    private fun handleHelloTouch(event: MotionEvent): Boolean {
+        return if (event.action == MotionEvent.ACTION_DOWN) {
+            cancelAnimationsAndCoroutines()
+            true
+        } else false
+    }
+    private fun handleParentLayoutTouch(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            cancelAnimationsAndCoroutines()
+            moveHelloTo(event.rawX, event.rawY)
+            setColorBasedOnLocale()
+            lifeCycleCoroutine = coroutineScope.launch {
+                delay(5000)
+                moveText()
             }
         }
-        binding.parentLayout.setOnTouchListener { view, event ->
-            when (event.action) {
-                MotionEvent.ACTION_DOWN -> {
+        return true
+    }
+    private fun cancelAnimationsAndCoroutines() {
+        animator?.cancel()
+        lifeCycleCoroutine?.cancel()
+    }
 
-                    animator?.cancel()
-                    lifeCycleCoroutine?.cancel()
-                    isMovingDown = true
+    private fun moveHelloTo(rawX: Float, rawY: Float) {
+        val newX = (rawX - binding.hello.width / 2).coerceIn(0f, binding.root.width - binding.hello.width.toFloat())
+        val newY = (rawY - binding.hello.height / 2).coerceIn(0f, binding.root.height - binding.hello.height.toFloat())
+        binding.hello.x = newX
+        binding.hello.y = newY
+    }
+    private fun setColorBasedOnLocale() {
+        val currentLocale = getCurrentLocale()
+        when (currentLocale) {
+            "en" -> binding.hello.setTextColor(Color.RED)
+            "ru" -> binding.hello.setTextColor(Color.BLUE)
 
-                    var newX = event.rawX - binding.hello.width / 2
-                    var newY = event.rawY - binding.hello.height / 2
+        }
+    }
 
-                    if (newX < 0) {
-                        newX = 0f
-                    } else if (newX + binding.hello.width > binding.root.width) {
-                        newX = binding.root.width - binding.hello.width.toFloat()
-                    }
-
-
-                    if (newY < 0) {
-                        newY = 0f
-                    } else if (newY + binding.hello.height > binding.root.height) {
-                        newY = binding.root.height - binding.hello.height.toFloat()
-                    }
-                    binding.hello.x = newX
-                    binding.hello.y = newY
-
-
-                    lifeCycleCoroutine = coroutineScope.launch {
-                        delay(5000)
-                        moveText()
-                    }
-
-
-                    val currentLocale: String =
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
-                            resources.configuration.locales[0].language
-                        } else {
-                            @Suppress("DEPRECATION")
-                            resources.configuration.locale.language
-                        }
-
-                    when (currentLocale) {
-                        "en" -> binding.hello.setTextColor(Color.RED)
-                        "ru" -> binding.hello.setTextColor(Color.BLUE)
-                    }
-
-                    view.performClick()
-                }
-            }
-            true
+    private fun getCurrentLocale(): String {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            resources.configuration.locales[0].language
+        } else {
+            @Suppress("DEPRECATION")
+            resources.configuration.locale.language
         }
     }
 
     private suspend fun moveText() {
         while (true) {
-            if (binding.hello.y >= binding.root.height - binding.hello.height) {
+            val endY = if (binding.hello.y >= binding.root.height - binding.hello.height) {
                 isMovingDown = false
-            }
-
-            if (binding.hello.y <= 0) {
-                isMovingDown = true
-            }
-
-            val endY = if (isMovingDown) {
-                binding.root.height - binding.hello.height.toFloat()
-            } else {
                 0f
+            } else {
+                isMovingDown = true
+                binding.root.height - binding.hello.height.toFloat()
             }
-            Log.d(
-                "AnimationDebug",
-                "Starting animation towards: ${if (isMovingDown) "Bottom" else "Top"}"
-            )
-            animator = ObjectAnimator.ofFloat(binding.hello, "y", binding.hello.y, endY)
-            animator?.duration = 10000
-            animator?.start()
-            animator?.suspendAndAwaitEnd()
+            animator = ObjectAnimator.ofFloat(binding.hello, "y", binding.hello.y, endY).apply {
+                duration = 10000
+                start()
+                suspendAndAwaitEnd()
+            }
             isMovingDown = !isMovingDown
         }
     }
-
     private suspend fun ObjectAnimator.suspendAndAwaitEnd() =
         suspendCancellableCoroutine { continuation ->
             addListener(object : AnimatorListenerAdapter() {
                 override fun onAnimationEnd(animation: Animator) {
                     removeListener(this)
-                    continuation.resume(Unit) {}
+                    continuation.resume(Unit)
                 }
             })
             continuation.invokeOnCancellation { cancel() }
@@ -146,7 +131,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onDestroy() {
         super.onDestroy()
-        lifeCycleCoroutine?.cancel()
+        cancelAnimationsAndCoroutines()
         coroutineScope.cancel()
     }
 }
